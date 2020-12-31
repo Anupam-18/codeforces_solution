f3acb0ee204f83673e2ebf3c3aedcfad307b4c0c
Anupam <anupam802154@gmail.com>
2020-12-31 20:30:25 +0530

    modified solution

--- solution.cpp
+++ solution.cpp
@@ -25,31 +25,6 @@
         	coordinate.pb({x,y});
         }
 
-        int ans=0;
-        for(int i=0; i< coordinate.size(); i++){
-        	bool left  = false;
-        	bool right = false;
-        	bool upper = false;
-        	bool lower = false;
-        	int target_x = coordinate[i].F;
-        	int target_y = coordinate[i].S;
-        	
-        	for(int j=0; j<coordinate.size(); j++){
-        		if(j==i)
-        		continue;
-        		if(coordinate[i].F == coordinate[j].F and coordinate[i].S > coordinate[j].S)
-        			lower = true;
-        		else if(coordinate[i].F == coordinate[j].F and coordinate[i].S < coordinate[j].S)
-        			upper = true;
-        		else if(coordinate[i].F < coordinate[j].F and coordinate[i].S == coordinate[j].S)
-        			right = true;
-        		else if(coordinate[i].F > coordinate[j].F and coordinate[i].S == coordinate[j].S)
-        			left = true;
-        	}
-        	if(lower and upper and right and left)
-        		ans++;
-        }
-        cout<<ans<<endl;
         
         return 0; 
     } 
