8c8c65bcfb04873c532bae66558d7fc0b331d1a9
Anupam <anupam802154@gmail.com>
2021-01-15 12:00:58 +0530

    Solved the problem and will add the readme later

--- solution.cpp
+++ solution.cpp
@@ -7,41 +7,49 @@
     #define all(a) begin(a),end(a)
     #define pb push_back
     #define mp make_pair
+    #define u_map unordered_map
+    #define map map
     #define S second
     #define F first
     #define MOD 1000000007
      
-      int main()
-      {
-        PKMKB;
-        lli n;
-        cin>>n;
      
-        unordered_map<lli,lli> vasya;
-        unordered_map<lli,lli> petya;
-        int loop = n;
-        int index=1;
-     
-        while(loop--){
-            lli temp;
-            cin>>temp;
-            vasya[temp] = index;
-            petya[temp] = n - index +1;
-            index++;
-        }
-     
-        lli queries;
-        cin>>queries;
-     
-        lli ans_vasya=0;
-        lli ans_petya=0;
-        
-        while(queries --){
-            lli q;
-            cin>>q;
-            ans_vasya += vasya[q];
-            ans_petya += petya[q];  
-        }
-        cout<<ans_vasya<<" "<<ans_petya<<endl;
+    int main() {
+       PKMKB;
+       int tc;
+       cin >> tc;
+       while(tc--)
+       {
+         
+          unordered_map<int,int> m;
+          lli x,y,z;
+          cin>>x>>y>>z;
+          m[x]++;
+          m[y]++;
+          m[z]++;
+          lli maximum=max(x,max(y,z));
+          if(m[maximum]<2)
+          cout<<"NO"<<endl;
+          else
+          {
+              int max2;
+    		if(x==maximum){
+    			if(y<maximum){
+    				max2=y;
+    			}
+    			else max2=z;
+    		}
+    		else if(y==maximum){
+    			if(x<maximum) max2=x;
+    			else max2=z;
+    		}
+    		else if(z==maximum){
+    			if(x<maximum) max2=x;
+    			else max2=y;
+    		}
+              cout<<"YES"<<endl;
+              cout<<maximum<<" "<<max2<<" "<<max2<<endl;
+          }
+       }
         return 0;
     }
